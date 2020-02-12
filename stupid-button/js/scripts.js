0edddfa86b7ac4c2f3ff3cf8c2ebb9a201a28d63
Katina Thongvong <kthongv1@w-dyn-210-243.dtcc.edu>
2020-02-12 15:46:26 -0500

    update script

--- stupid-button/js/scripts.js
+++ stupid-button/js/scripts.js
@@ -4,4 +4,6 @@ yeeButton.addEventListener("click", playSound);
 
 function playSound() {
   console.log("you called the playSound function!");
+  var beep = new Audio("beep.wav");
+  beep.play();
 }
