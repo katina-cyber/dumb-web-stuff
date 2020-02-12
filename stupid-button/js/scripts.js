6ca0827e2db111fd62c7b250794891a457b853a8
Katina Thongvong <kthongv1@w-dyn-210-243.dtcc.edu>
2020-02-12 15:38:29 -0500

    update script

---
+++ stupid-button/js/scripts.js
@@ -0,0 +1,7 @@
+var yeeButton = document.getElementById("yee-button");
+
+yeeButton.addEventListener("click", playSound);
+
+function playSound() {
+  console.log("you called the playSound function!");
+}
